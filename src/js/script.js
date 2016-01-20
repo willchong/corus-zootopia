cc161c6555fc647f7de478b3f675d69529ef840a
William Chong <williamc@tribalworldwide.ca>
2016-01-20 00:10:52 -0500

    styling, some responsive video js stuff, form updates

--- src/js/script.js
+++ src/js/script.js
@@ -1,35 +1,23 @@
-$( document ).ready(function() {
+$(document).ready(function() {
 
-	$('.video-carousel').slick({
-	  infinite: true,
-	  slidesToShow: 3,
-	  centerMode: false,
-	  slidesToScroll: 1
-	});
+	var resizedHeight = $('.video').width();
+	resizedHeight = resizedHeight*(635/992);
+	$('.video').css('height', resizedHeight);
 
-	$('.video-thumbnail').on('click', function(){
+	var resizedWidth = $('.video').width();
+	resizedWidth = resizedWidth*(703/992);
+	$('.video-footer-container').css('width', resizedWidth);
 
-		console.log($(this).attr('data-video'));
-		var theVideo = $(this).attr('data-video');
-		var theSource = '';
+});
 
-		switch(theVideo) {
-			case '1':
-				theSource = '8EKrtSx8fzE';
-				break;
-			case '2':
-				theSource = 'qWiFVM2zTvg';
-				break;
-			case '3':
-				theSource = 'L9WLMiHGOqU';
-				break;
-		}
+$(window).on('resize', function() {
 
-		var theEmbed = 'https://www.youtube.com/embed/'+theSource+'?controls=0&showinfo=0&rel=0&autoplay=1';
+	var resizedHeight = $('.video').width();
+	resizedHeight = resizedHeight*(635/992);
+	$('.video').css('height', resizedHeight);
 
-		$('.video iframe').attr('src', '');
-		$('.video iframe').attr('src', theEmbed);
-
-	});
+	var resizedWidth = $('.video').width();
+	resizedWidth = resizedWidth*(703/992);
+	$('.video-footer-container').css('width', resizedWidth);
 
 });
